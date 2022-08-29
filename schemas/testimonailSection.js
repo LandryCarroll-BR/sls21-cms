35463c65b97f5ec6c6e10a6d3dcd7be1e2f83580
landtry <landry@boldist.co>
2022-08-29 07:33:23 -0500

    fixed typo on testimonial

--- schemas/testimonailSection.js
+++ schemas/testimonailSection.js
@@ -7,19 +7,19 @@ export default {
       name: "column1",
       title: "Column 1",
       type: "array",
-      of: [{ type: "reference", to: { type: "testimonal" } }],
+      of: [{ type: "reference", to: { type: "testimonial" } }],
     },
     {
       name: "column2",
       title: "Column 2",
       type: "array",
-      of: [{ type: "reference", to: { type: "testimonal" } }],
+      of: [{ type: "reference", to: { type: "testimonial" } }],
     },
     {
       name: "column3",
       title: "Column 3",
       type: "array",
-      of: [{ type: "reference", to: { type: "testimonal" } }],
+      of: [{ type: "reference", to: { type: "testimonial" } }],
     },
   ],
 };
